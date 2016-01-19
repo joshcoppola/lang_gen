4da8663b463f6fdc7a3544070ffb5651c6926f84
Josh <joshuac@tyemill.com>
2016-01-18 23:23:52 -0800

    Create a dictionary to be used later to help determine word function and length

---
+++ dictionary.py
@@ -0,0 +1,17 @@
+
+''' 
+For now, this file will be used to store data related to words
+'''
+
+PREPOSITIONS = {'aboard', 'about', 'above', 'across', 'after', 'against', 'along',
+    'amid', 'among', 'anti', 'around', 'as', 'at', 'before', 'behind', 'below', 'beneath',
+    'beside', 'besides', 'between', 'beyond', 'but', 'by', 'concerning', 'considering', 
+    'despite', 'down', 'during', 'except', 'excepting', 'excluding', 'following', 'for',
+    'from', 'in', 'inside', 'into', 'like', 'minus', 'near', 'of', 'off', 'on', 'onto', 
+    'opposite', 'outside', 'over', 'past', 'per', 'plus', 'regarding', 'round', 'save',
+    'since', 'than', 'through', 'to', 'toward', 'towards', 'under', 'underneath', 'unlike',
+    'until', 'up', 'upon', 'versus', 'via', 'with', 'within', 'without'
+    }
+    
+    
+ARTICLES = {'a', 'an', 'the'}
