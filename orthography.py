c2c26c988eb51ea3ece5193e3f765d6ffad38de7
Joshua Coppola <joshuacoppola@Joshuas-MacBook-Pro.local>
2016-01-21 20:48:06 -0800

    Begin rewrite of how orthographies pick graphemes

--- orthography.py
+++ orthography.py
@@ -127,50 +127,50 @@ PHONEMES_WRITTEN = {
 
     # ----------- VOWELS ----------- #
 
-    101:{'i':3,   i_u:1,   i_c:1,  i_l:1,  i_r:1},                      # sit
-    102:{'e':2,   'ea':2,  e_r:1,  e_c:1,  e_u:1, e_l:1, i_l:1, i_r:1}, # see
-    103:{'u':3,   u_u:1,   u_c:1,  u_l:1,  u_r:1},                      # up
-    104:{'e':3,   e_r:1,   e_c:1,  e_u:1,  e_l:1},                      # beg
-    105:{'a':3,   ae:1,    a_c:1,  a_u:1,  a_l:1, a_o:1, a_r:1},        # bad
-    106:{'ae':3,  a_c:1,   a_u:1,  a_l:1,  a_o:1, a_r:1},               # sundae
-    107:{'a':2,   'aa':2,  a_c:1,  a_u:1,  a_l:1, a_o:1, a_r:1},        # Saab
-    108:{'ie':1,  'i':1,   i_u:1,  i_c:1,  i_l:1, i_r:1},               # pie
-    109:{'o':1,   o_c:1,   o_u:1,  o_l:1,  o_r:1},                      # toe
-    110:{'u':1,   'eu':2,  'eo':2, u_u:1,  u_c:1, u_l:1, u_r:1},        # good
-    111:{'ue':2,  u_u:1,   u_c:1,  u_l:1,  u_r:1},                      # blue
-    112:{'au':1},                                                       # auger
-    113:{'ou':1},                                                       # out
-    114:{'oi':1},                                                       # toil
+    101:'i',  # sit
+    102:'e',  # see      'ea'
+    103:'u',  # up
+    104:'e',  # beg
+    105:'a',  # bad
+    106:'a',  # sundae   'ae'
+    107:'a',  # Saab     'aa'
+    108:'i',  # pie      'ie'
+    109:'o',  # toe
+    110:'u',  # good      'eu'  'eo'
+    111:'u',  # blue      'ue'
+    112:'au', # auger
+    113:'ou', # out
+    114:'oi', # toil
 
     # ----------- Consonants ----------- #
 
-    300:{'':1}, # (300 and 301 are special - used at syllable onsets which don't start with
-    301:{'':1}, # a consonant and syllable codas which don't end with a consonant, respectively
+    300:'', # (300 and 301 are special - used at syllable onsets which don't start with
+    301:'', # a consonant and syllable codas which don't end with a consonant, respectively
 
-    201:{'p':1},
-    202:{'b':1},
-    203:{'t':1},
-    204:{'d':1},
-    205:{'k':4, 'q':1, 'c':2},
-    206:{'g':1},
-    207:{'ch':15, 'c':3, c_s:1},
-    208:{'j':1, 'g':1},
-    209:{'f':4, 'ph':1},
-    210:{'v':1},
-    211:{'th':1},
-    212:{'th':1},
-    213:{'s':1},
-    214:{'z':1},
-    215:{'sh':1},
-    216:{'zh':1, 'z':2, c_s:1},
-    217:{'h':1},
-    218:{'m':1},
-    219:{'n':1},
-    220:{'ng':2, chr(237):1, chr(238):1},
-    221:{'r':1},
-    222:{'y':1},
-    223:{'w':1},
-    224:{'l':1}
+    201:'p',
+    202:'b',
+    203:'t',
+    204:'d',
+    205:'k',  # 'q' 'c'
+    206:'g',
+    207:'ch', # 'c' c_s
+    208:'j',  # 'g'
+    209:'f',  # 'ph'
+    210:'v',
+    211:'th',
+    212:'th',
+    213:'s',
+    214:'z',
+    215:'sh',
+    216:'zh', # 'z' c_s
+    217:'h',
+    218:'m',
+    219:'n',
+    220:'ng', # chr(237) chr(238)
+    221:'r',
+    222:'y',
+    223:'w',
+    224:'l'
     }
 
 
