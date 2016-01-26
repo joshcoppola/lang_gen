091757d1fee805d62945509f6966e8ec905bb577
Joshua Coppola <joshuacoppola@Joshuas-MacBook-Pro.local>
2016-01-25 16:31:48 -0800

    Add deeper business logic to determining syllable onsets and codas

--- lang_gen.py
+++ lang_gen.py
@@ -4,6 +4,9 @@ import random
 
 from phonemes import CONSONANTS, VOWELS, POSSIBLE_ONSETS, POSSIBLE_CODAS, EMPTY_CONSONANTS
 
+import orthography
+
+
 ''' 
 This file generates languages (and eventually orthographies) which have distinct
 phonemes and orthographies.
@@ -18,7 +21,7 @@ class Language:
         self.vowel_probabilities_by_following_cluster = {}
 
         self.vocabulary = {}
-        self.orthography = None
+        self.orthography = orthography.Orthography()
 
         self.valid_vowels = set()
         self.valid_consonants = set()
@@ -32,11 +35,11 @@ class Language:
             
         # Start off by randomly discarding some phonemes that will never be used in this language
         for c in CONSONANTS:
-            if roll(1, 100) > 10 and c.num < 300:
+            if roll(1, 100) > 15 and c.num < 300:
                 self.valid_consonants.add(c)
 
         for v in VOWELS:
-            if roll(1, 100) > 10: #and v.type != 'diphthong':
+            if roll(1, 100) > 15: #and v.type != 'diphthong':
                 self.valid_vowels.add(v)
 
         # Now, figure out probabilities for each of the onsets and codas
@@ -82,20 +85,14 @@ class Language:
     def create_word(self, syllables=2):
         ''' Generate a word in the language, using the appropriate phoneme frequencies '''
         
-        word = ''
+        word = []
+        # Set to None so that the first onset knows that it's word-initial (no coda comes before the first syllable)
+        coda = None
 
-        previous_coda_complex = 0
-        
-        for i in xrange(syllables):
+        for i in xrange(syllables):            
             
-            ## -------- Handle onset (none if previous coda was complex) ------- ##
-            if not previous_coda_complex: onset = weighted_random(self.onset_probabilities)
-            else:                         onset = EMPTY_CONSONANTS[0]
-            ## ----------------------------------------------------------------- ##
-
-            coda = weighted_random(self.coda_probabilities)
-            # Update the flag for whether the previous coda was complex or not
-            previous_coda_complex = 1 if len(coda.consonant_array) > 1 else 0
+            onset = self.choose_valid_onset(previous_coda=coda)
+            coda  = self.choose_valid_coda(onset=onset)
 
             # Find the probabilities of each vowel occuring based on the phoneme clusters surrounding it
             vowel_probabilities = {v: self.vowel_probabilities_by_preceding_cluster[v][onset] + 
@@ -103,12 +100,65 @@ class Language:
 
             vowel = weighted_random(vowel_probabilities)
 
+            word.extend([c.num for c in onset.consonant_array])
+            word.append(vowel.num)
+            word.extend([c.num for c in coda.consonant_array])
+            # word += '{0}{1}{2}'.format(onset.get_string(), vowel.get_string(), coda.get_string())
 
-            word += '{0}{1}{2}'.format(onset.get_string(), vowel.get_string(), coda.get_string())
+        self.orthography.phon_to_orth(phoneme_sequence=word)
+        # print ''.join([orthography.PHONEMES_WRITTEN[phoneme] for phoneme in word])
 
 
-        
-        print word
+    def choose_valid_onset(self, previous_coda):
+        ''' Business logic for determining whether an onset is valid, given the previous coda and other constraints '''
+
+        # At the beginning of the word, any onset is valid
+        if previous_coda is None:
+            onset = weighted_random(self.onset_probabilities)
+
+        # Otherwise, if the previous coda was complex, we'll assign an empty onset
+        elif previous_coda.is_complex():
+            onset = EMPTY_CONSONANTS[0]
+
+        # Otherwise, generate an onset with some restrictions
+        else:
+            # Stash the previous coda's last phoneme in case we need to check it multiple times
+            previous_coda_last_phoneme = previous_coda.consonant_number_array[-1]
+
+            # Loop through and generate onsets until one matches all criteria
+            while True:
+                onset = weighted_random(self.onset_probabilities)
+
+                # Can't start one syllable off with the same phoneme that the previous ended with
+                if onset.consonant_number_array[-1] == previous_coda_last_phoneme:
+                    continue
+
+                # Can't have an open coda followed by an empty onset
+                if onset.is_empty() and previous_coda.is_empty():
+                    continue
+
+                break
+
+        return onset
+
+    def choose_valid_coda(self, onset):
+        ''' Business logic for determining whether a coda is valid, given the syllable onset and other constraints '''
+
+        # No /l/ or /r/ in codas when the onset contains one of these 
+        restrict_rl = onset.has_any_phoneme( (221, 224) )
+
+        while True:
+            coda = weighted_random(self.coda_probabilities)
+
+            # No /l/ or /r/ in codas when the onset contains one of these 
+            if restrict_rl and coda.has_any_phoneme( (221, 224)):
+                continue
+
+            # If the coda has made it through the gauntlet, break out of the loop and return it
+            break
+
+        return coda
+
 
     def info_dump(self):
         print '\n Onsets \n -', sum(self.onset_probabilities.values())
@@ -160,14 +210,16 @@ def generate_pclusters():
             # print c.get_string(), c.rule_set
 
 if __name__ == '__main__':
+    print ''
     generate_pclusters()
 
     t = Language()
     t.generate_language_properties()
 
-    t.info_dump()
+    # t.info_dump()
     
     for i in xrange(20):
         t.create_word(syllables=roll(1, 2))
 
+    print ''
 
